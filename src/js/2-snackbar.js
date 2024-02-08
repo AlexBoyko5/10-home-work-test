463bd63037295f59609b05a4d39b8e2e9b58eb21
AlexBoyko5 <144361285+AlexBoyko5@users.noreply.github.com>
2024-02-08 19:14:25 +0100

    commit#3
    all tasks done

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,92 +1,134 @@
 //~==== Вариант МОЙ ===============================================
-
-//~==== Вариант Alex ===============================================
-
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-const form = document.querySelector('.form');
+const form = document.querySelector(".form");
 
-function delayFunction(delay, state) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (state === 'fulfilled') {
-                resolve(`Promise resolved after ${delay} milliseconds`);
-            } else {
-                reject(`Promise rejected after ${delay} milliseconds`);
-            }
-        }, delay);
-    });
-}
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
+    const delayInput = form.querySelector('input[name="delay"]');
+    const delay = parseInt(delayInput.value);
 
-    const delay = parseInt(e.target.elements.delay.value, 10);
-    const state = e.target.elements.state.value;
-
-    delayFunction(delay, state)
-        .then(result => {
-            console.log(result);
-            iziToast.success({
-                title: '',
-                message: `${result}`,
-                position: 'topRight',
-            });
-            e.target.elements.delay.value = '';
-            e.target.elements.state.value = '';
-        })
-        .catch(err => {
-            console.log(err);
-            iziToast.error({
-                title: '',
-                message: `${err}`,
-                position: 'topRight',
-            });
-        });
-    e.target.elements.delay.value = '';
-    e.target.elements.state.value = '';
-});
-
-//~==== Вариант Лиля ===============================================
-import iziToast from 'izitoast';
-
-import 'izitoast/dist/css/iziToast.min.css';
-
-const formEl = document.querySelector('.form');
-formEl.addEventListener('submit', createPromise);
-
-function createPromise(e) {
-    e.preventDefault();
-
-    const storedObj = {
-        delay: parseInt(formEl.elements.delay.value),
-        state: formEl.elements.state.value,
-    };
+    const stateInput = form.querySelector('input[name="state"]:checked');
+    const state = stateInput.value;
 
     const promise = new Promise((resolve, reject) => {
         setTimeout(() => {
-            if (storedObj.state === 'fulfilled') {
-                resolve(`✅ Fulfilled promise in ${storedObj.delay}ms`);
+            if (state === "fulfilled") {
+                resolve(delay);
             } else {
-                reject(`❌ Rejected promise in ${storedObj.delay}ms`);
+                reject(delay);
             }
-        }, storedObj.delay);
+        }, delay);
     });
 
-    promise
-        .then(result => {
+    promise.then(
+        (delay) => {
             iziToast.success({
-                position: 'topRight',
-                message: result,
+                title: "Success",
+                message: `✅ Fulfilled promise in ${delay}ms`,
             });
-        })
-        .catch(error => {
+        },
+        (delay) => {
             iziToast.error({
-                position: 'topRight',
-                message: error,
+                title: "Error",
+                message: `❌ Rejected promise in ${delay}ms`,
             });
-        });
-}
+        }
+    );
+});
+
+
+//~==== Вариант Alex ===============================================
+
+// import flatpickr from "flatpickr";
+// import "flatpickr/dist/flatpickr.min.css";
+// import iziToast from "izitoast";
+// import "izitoast/dist/css/iziToast.min.css";
+
+// const form = document.querySelector('.form');
+
+// function delayFunction(delay, state) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (state === 'fulfilled') {
+//                 resolve(`Promise resolved after ${delay} milliseconds`);
+//             } else {
+//                 reject(`Promise rejected after ${delay} milliseconds`);
+//             }
+//         }, delay);
+//     });
+// }
+
+// form.addEventListener('submit', e => {
+//     e.preventDefault();
+
+//     const delay = parseInt(e.target.elements.delay.value, 10);
+//     const state = e.target.elements.state.value;
+
+//     delayFunction(delay, state)
+//         .then(result => {
+//             console.log(result);
+//             iziToast.success({
+//                 title: '',
+//                 message: `${result}`,
+//                 position: 'topRight',
+//             });
+//             e.target.elements.delay.value = '';
+//             e.target.elements.state.value = '';
+//         })
+//         .catch(err => {
+//             console.log(err);
+//             iziToast.error({
+//                 title: '',
+//                 message: `${err}`,
+//                 position: 'topRight',
+//             });
+//         });
+//     e.target.elements.delay.value = '';
+//     e.target.elements.state.value = '';
+// });
+
+//~==== Вариант Лиля ===============================================
+// import iziToast from 'izitoast';
+
+// import 'izitoast/dist/css/iziToast.min.css';
+
+// const formEl = document.querySelector('.form');
+// formEl.addEventListener('submit', createPromise);
+
+// function createPromise(e) {
+//     e.preventDefault();
+
+//     const storedObj = {
+//         delay: parseInt(formEl.elements.delay.value),
+//         state: formEl.elements.state.value,
+//     };
+
+//     const promise = new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (storedObj.state === 'fulfilled') {
+//                 resolve(`✅ Fulfilled promise in ${storedObj.delay}ms`);
+//             } else {
+//                 reject(`❌ Rejected promise in ${storedObj.delay}ms`);
+//             }
+//         }, storedObj.delay);
+//     });
+
+//     promise
+//         .then(result => {
+//             iziToast.success({
+//                 position: 'topRight',
+//                 message: result,
+//             });
+//         })
+//         .catch(error => {
+//             iziToast.error({
+//                 position: 'topRight',
+//                 message: error,
+//             });
+//         });
+// }
